e467fde18ad3460e8632c1e3986b3359f9e39fff
Phil-hacker <61991187+Phil-hacker@users.noreply.github.com>
2024-05-10 13:19:14 +0200

    removed old serviceworker

--- assets/sw.js
+++ assets/sw.js
@@ -1,25 +1,13 @@
-var cacheName = 'egui-template-pwa';
-var filesToCache = [
-  './',
-  './index.html',
-  './supertictactoe.js',
-  './supertictactoe.wasm',
-];
-
-/* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+    self.skipWaiting();
 });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('activate', function (e) {
+    self.registration.unregister()
+        .then(function () {
+            return self.clients.matchAll();
+        })
+        .then(function (clients) {
+            clients.forEach(client => client.navigate(client.url))
+        });
 });
